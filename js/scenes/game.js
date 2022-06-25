65fafe3c1dc4b12dbd4585eb3e13824b7b181eea
u1973226 <u1973226@campus.udg.edu>
2022-06-25 15:21:55 +0200

    Upload 2
    wewe

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -3,10 +3,11 @@ class GameScene extends Phaser.Scene {
         super('GameScene');
 		this.score = 0;
 		this.froot_n_poison = null;
-		this.pear = null;
+		//this.pear = null; The pear
 		this.rand_time_appearition = 0;
 		this.timer;
 		this.text;
+		this.add_point = 0;
 		this.game_over = false;
 		this.array_objects = ['Apple', 'Banana', 'Pear', 'Strawberry', 'Poison'];
     }
@@ -19,26 +20,22 @@ class GameScene extends Phaser.Scene {
 		this.load.image('Poison', '../resources/Poison but clearer.png');
 	}
 
-    create (){	
+    create (){
+		this.froot_n_poison = this.physics.add.group();
 		this.rand_time_appearition = Phaser.Math.RND.between(2, 5);
 		this.cameras.main.setBackgroundColor(0xBFFCFF);
 
-		this.text = this.add.text(32, 32);
-		this.text2 = this.add.text(64, 64);
 		this.timer = this.time.delayedCall(0, timerCall, [], this);
-		/*
-		this.timer = this.time.addEvent({ delay: 5000, repeat: 2, loop: true});
-		 this.add.text(25, 25, this.timer.time, {fontSize: '32px', fill: '#000'});
-		*/
+		
+		this.text = this.add.text(25, 550, "Score: " + this.score, {fontSize: '32px', fill: '#000'});
 
-		let score =  "Score: " + this.score;
-		this.add.text(25, 550, score, {fontSize: '32px', fill: '#000'});
-
-		this.pear = this.physics.add.sprite(100, 100, 'Pear');
+		//This pear was the first falling thing I made. In honor to pear, I decided to keep it in code
+		/*this.pear = this.physics.add.sprite(100, 100, 'Pear');
 		this.pear.setBounce(0.2);
-		this.pear.setCollideWorldBounds(true);
+		this.pear.setCollideWorldBounds(true);*/
 
-		this.exitButton = this.add.text(310, 500, 'Go to menu', {fontSize: '32px', fill:'#000'})
+		if (this.game_over) {
+			this.exitButton = this.add.text(310, 500, 'Go to menu', {fontSize: '32px', fill:'#000'})
 			.setInteractive({ useHandCursor: true })
 			.on('pointerover', () => this.exitButton.setStyle({ fill: 'grey'}))
 			.on('pointerout', () => {
@@ -55,21 +52,33 @@ class GameScene extends Phaser.Scene {
 				this.exitButton.setPosition(310, 500)
 				loadpage("../")
 			});
+		}
 			
-		let game_over = false;
+		
 		let i = 0;
 		
 	}
 	
 	update (){
-		this.text.setText('Event Progress: ' + this.timer.getProgress().toString().substr(0, 4));
+		this.text.setText("Score: " + this.score);
 	}
 
 
 }	
 
 function timerCall() {
-	this.physics.add.sprite(Phaser.Math.RND.between(25, 775), 100, this.array_objects[Phaser.Math.RND.between(0, 4)])
+	let random_thing = this.array_objects[Phaser.Math.RND.between(0, 4)];
+	this.froot_n_poison.create(Phaser.Math.RND.between(25, 775), -100, random_thing);
+	this.froot_n_poison.children.iterate((thing) => {
+		thing.name = random_thing;
+		thing.setInteractive({ useHandCursor: true });
+		thing.on('pointerdown', () => {
+			if (!this.game_over && thing.name != 'Poison') {
+				thing.disableBody(true,true);
+				this.score = this.score + 1;
+			}
+		})
+	});
 	if (!this.game_over) {
 		this.timer = this.time.delayedCall(Phaser.Math.RND.between(500, 2000), timerCall, [], this);
 	}
